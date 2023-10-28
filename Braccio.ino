52422a8b8bb2d02bb1b8c61f92283e39ef581de9
HLmmw <37757289+HLmmw@users.noreply.github.com>
2023-10-28 09:32:19 +0700

    add simpleMovements code from Andrea Martino

--- Braccio.ino
+++ Braccio.ino
@@ -1,8 +1,21 @@
 #include <Braccio.h>
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
 
 void setup(){
+  Braccio.begin();
 }
 
 void loop(){
+  Braccio.ServoMovement(20, 0, 15, 180, 170, 0, 73);
+  delay(1000);
+  Braccio.ServoMovement(20, 180, 165, 0, 0, 180, 10);
+  delay(1000);
 }
 
