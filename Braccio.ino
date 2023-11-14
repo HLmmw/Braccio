5d5090ce98916e35cc9a1a2344040564b18b6c5c
HLmmw <hartonoindra@gmail.com>
2023-11-14 19:09:44 +0700

    add LED class and mark BraccioControl class as comment

--- Braccio.ino
+++ Braccio.ino
@@ -3,11 +3,17 @@
 #include <Servo.h>
 
 Servo base;
-const int numServos = 6; // Number of servo motors
-Servo servos[numServos]; // Array of servo objects
-int servoAngles[numServos]; // Array to store servo angles
-int servoLedPin[numServos -1] = {2, 4, 7, 8 ,13}; // Array of servo LED indicators
-int selectedServo = 0; // Currently selected servo (o o numServos -1)
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
+//const int numServos = 6; // Number of servo motors
+//Servo servos[numServos]; // Array of servo objects
+//int servoAngles[numServos]; // Array to store servo angles
+//int servoLedPin[numServos -1] = {2, 4, 7, 8 ,13}; // Array of servo LED indicators
+//int selectedServo = 0; // Currently selected servo (o o numServos -1)
 
 const int basePin = 11;
 const int shoulderPin = 10;
@@ -43,48 +49,75 @@ private:
   unsigned long debounceDelay;
 };
 
-class BraccioControl {
-public:
-  BraccioControl() {
+//class BraccioControl {
+//public:
+//  BraccioControl() {
     // Initialize all servo angles to 90 degrees
     //for (int i=0; i<numServos; i++) {
     //servoAngles[i] = 90;
     //}
-    servoAngles[0] = 0;
-    servoAngles[1] = 40;
-    servoAngles[2] = 180;
-    servoAngles[3] = 170;
-    servoAngles[4] = 0;
-    servoAngles[5] = 73;
-   }
+//    servoAngles[0] = 0;
+//    servoAngles[1] = 40;
+//    servoAngles[2] = 180;
+//    servoAngles[3] = 170;
+//    servoAngles[4] = 0;
+//    servoAngles[5] = 73;
+//   }
   
-  void attachServos(int basePin, int shoulderPin, int elbowPin, int wristRotPin, int wristVerPin, int gripperPin) {
+//  void attachServos(int basePin, int shoulderPin, int elbowPin, int wristRotPin, int wristVerPin, int gripperPin) {
     // Attach servo objects to GPIO pins
-    servos[0].attach(basePin);
-    servos[1].attach(shoulderPin);
-    servos[2].attach(elbowPin);
-    servos[3].attach(wristRotPin);
-    servos[4].attach(wristVerPin);
-    servos[5].attach(gripperPin);
-  }
+//    servos[0].attach(basePin);
+//    servos[1].attach(shoulderPin);
+//    servos[2].attach(elbowPin);
+//    servos[3].attach(wristRotPin);
+//    servos[4].attach(wristVerPin);
+//    servos[5].attach(gripperPin);
+//  }
   
-  void selectNextServo() {
-    selectedServo = (selectedServo + 1) % numServos; // Cycle through servos
-    delay(200); // Debounce delay
-  }
+//  void selectNextServo() {
+//    selectedServo = (selectedServo + 1) % numServos; // Cycle through servos
+//    delay(200); // Debounce delay
+//  }
   
-  void moveSelectedServo(int angle) {
-    servoAngles[selectedServo] = constrain(angle, 0, 180);
-    servos[selectedServo].write(servoAngles[selectedServo]);
-  }
+//  void moveSelectedServo(int angle) {
+//    servoAngles[selectedServo] = constrain(angle, 0, 180);
+//    servos[selectedServo].write(servoAngles[selectedServo]);
+//  }
   
-  int getSelectedServoAngle() {
-    return servoAngles[selectedServo];
-  }
+//  int getSelectedServoAngle() {
+//    return servoAngles[selectedServo];
+//  }
   
-};
+//};
 
-BraccioControl tinkerkit;
+//BraccioControl tinkerkit;
+
+class LED {
+private:
+  int pin;
+  boolean isOn;
+  
+public:
+  LED(int pinNumber) {
+    pin = pinNumber;
+    isOn = false;
+    pinMode(pin, OUTPUT);
+  }
+  
+  void turnOn() {
+    digitalWrite(pin, HIGH);
+    isOn = true;
+  }
+  
+  void turnOff() {
+    digitalWrite(pin, LOW);
+    isOn = false;
+  }
+  
+  boolean isLit() {
+    return isOn;
+  }  
+};
 
 Button redButton(15);
 Button yellowButton(17);
@@ -92,16 +125,22 @@ Button greenButton(18);
 Button blueButton(16);
 Button whiteButton(19);
 
+LED baseLED(2);
+LED shoulderLED(4);
+LED elbowLED(7);
+LED wristVLED(8);
+LED wristRLED(13);
+
 void setup(){
 
   Serial.begin(9600);
-  base.attach(11);
+  Braccio.begin();
   
-  tinkerkit.attachServos(basePin, shoulderPin, elbowPin, wristRotPin, wristVerPin, gripperPin);
+//  tinkerkit.attachServos(basePin, shoulderPin, elbowPin, wristRotPin, wristVerPin, gripperPin);
 
-  for (int i=0; i<numServos-1; i++) {
-    pinMode(servoLedPin[i], OUTPUT);
-  }
+//  for (int i=0; i<numServos-1; i++) {
+//    pinMode(servoLedPin[i], OUTPUT);
+//  }
 
 //  Braccio.begin();
 
@@ -112,23 +151,25 @@ void loop(){
   int angle;
   
   if (yellowButton.isPressed()) {
-    int angle = tinkerkit.getSelectedServoAngle();
+//    int angle = tinkerkit.getSelectedServoAngle();
     Serial.println("yellow button pressed, angle = " + String(angle));
-    tinkerkit.selectNextServo();
-    Serial.println("selected servo = " + String(selectedServo));
-    Serial.println("servo angle = " + String(servoAngles[selectedServo]));
+//    tinkerkit.selectNextServo();
+//    Serial.println("selected servo = " + String(selectedServo));
+//    Serial.println("servo angle = " + String(servoAngles[selectedServo]));
   }
   
   if (redButton.isPressed()) {
-    Serial.println("red button pressed, angle = " + String(angle));
-    angle += 5;
-    base.write(angle);
+    base.write(80);
+//    Serial.println("red button pressed, angle = " + String(angle));
+//    angle += 5;
+//    base.write(angle);
 //    tinkerkit.moveSelectedServo(angle);
   } 
   else if (greenButton.isPressed()) {
-    Serial.println("green button pressed, angle = " + String(angle));
-    angle -= 5;
-    base.write(angle);
+    base.write(180);
+//    Serial.println("green button pressed, angle = " + String(angle));
+//    angle -= 5;
+//    base.write(angle);
 //    tinkerkit.moveSelectedServo(angle);
   }
 
